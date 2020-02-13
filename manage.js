e5ff5984911838227d9c2ba3b04cf23e0a77832a
DiegoPerez1441 <diegooperez14@gmail.com>
2020-02-12 20:06:55 -0600

    Added nav bar

--- manage.js
+++ manage.js
@@ -2,25 +2,25 @@
 $(document).ready(function() {
 
   var $element = $('.parallax');
-    // Call Function on scroll  
-    $(window).scroll(function () {
-        parallax($element);
+  // Call Function on scroll  
+  $(window).scroll(function () {
+      parallax($element);
+  });
+  parallax($element);
+
+  var speed = 0.2;
+  function parallax($element) {
+    // Difference
+    var diff = $(window).scrollTop() - $element.offset().top;
+
+    // New y position
+    var yPos = -(diff * speed);
+    // Calculate new y coordinates
+    var coords = '50% ' + yPos + 'px';
+    // Update coordinates in the CSS
+    $element.css({
+      backgroundPosition: coords
     });
-    parallax($element);
-
-    var speed = 0.2;
-    function parallax($element) {
-      // Difference
-      var diff = $(window).scrollTop() - $element.offset().top;
-
-      // New y position
-      var yPos = -(diff * speed);
-      // Calculate new y coordinates
-      var coords = '50% ' + yPos + 'px';
-      // Update coordinates in the CSS
-      $element.css({
-        backgroundPosition: coords
-      });
-    }
+  }
 
 });
