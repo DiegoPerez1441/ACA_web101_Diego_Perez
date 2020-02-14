0cfa5cf20aa9cbbc9835144a87b453036364f33e
DiegoPerez1441 <diegooperez14@gmail.com>
2020-02-14 10:42:38 -0600

    Fixed Bugs *Need to finish fixing Parallax bug*

--- manage.js
+++ manage.js
@@ -7,26 +7,16 @@
 // Wait for the document to load before jQuery starts
 $(document).ready(function() {
 
-  var $element = $('.parallax');
   // Call Function on scroll  
   $(window).scroll(function () {
-      parallax($element);
+    parallax();
   });
-  parallax($element);
+  
+  function parallax() {
+    var wScroll = $(window).scrollTop();
 
-  var speed = 0.2;
-  function parallax($ele) {
-    // Difference
-    var diff = $(window).scrollTop() - $ele.offset().top;
+    $('.parallax').css('background-position', 'center '+(wScroll*0.5)+'px')
 
-    // New y position
-    var yPos = -(diff * speed);
-    // Calculate new y coordinates
-    var coords = '50% ' + yPos + 'px';
-    // Update coordinates in the CSS
-    $ele.css({
-      backgroundPosition: coords
-    });
   }
 
 });
