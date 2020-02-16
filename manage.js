2db1d7f74bcc07fb7280dd38bd15d8808c9e7ed3
DiegoPerez1441 <devdiegoperez@gmail.com>
2020-02-15 19:39:46 -0600

    Removed Parallax Effect from Mobile

--- manage.js
+++ manage.js
@@ -24,25 +24,29 @@ $(document).ready(function() {
   // Call Function on scroll  
   $(window).scroll(function () {
 
-    var scrolled = $(window).scrollTop();
-    // Loop through every element that contains the class 'parallax'
-    // so that every element has its own info/properties
-    $('.parallax').each(function(index, element) {
-      // offset from top of the page
-      var initY = $(this).offset().top;
-      var height = $(this).height();
-      var endY  = initY + $(this).height();
+    // Prevent the parallax effect from running on a mobile device
+    if (screen.width >= 500) {
+      var scrolled = $(window).scrollTop();
+      // Loop through every element that contains the class 'parallax'
+      // so that every element has its own info/properties
+      $('.parallax').each(function(index, element) {
+        // offset from top of the page
+        var initY = $(this).offset().top;
+        var height = $(this).height();
+        var endY  = initY + $(this).height();
+  
+        // Check if the element is in the viewport, then proceed
+        var visible = isInViewport(this);
+        if(visible) {
+          var diff = scrolled - initY;
+          // ratio of where the element is from its total height
+          var ratio = Math.round((diff / height) * 100);
+          // Update CSS by changing the elements' coordinates to the ratio of the elements' scroll value times the speed
+          $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px');
+        }
+      })
+    }
 
-      // Check if the element is in the viewport, then proceed
-      var visible = isInViewport(this);
-      if(visible) {
-        var diff = scrolled - initY;
-        // ratio of where the element is from its total height
-        var ratio = Math.round((diff / height) * 100);
-        // Update CSS by changing the elements' coordinates to the ratio of the elements' scroll value times the speed
-        $(this).css('background-position','center ' + parseInt(-(ratio * 1.5)) + 'px');
-      }
-    })
   });
   
 });
